7c722c4c11b0042f278e99b78ce980ac852cfeb4
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-11-29 19:30:32 -0600

    Add files via upload

--- diy-mini.sh
+++ diy-mini.sh
@@ -128,14 +128,6 @@ chmod 755 package/base-files/files/bin/coremark.sh
 cp -f $GITHUB_WORKSPACE/configfiles/swconfig_install package/base-files/files/etc/init.d/swconfig_install
 chmod 755 package/base-files/files/etc/init.d/swconfig_install
 
-# 电工大佬的rtl8367b驱动资源包，暂时使用这样替换
-wget https://github.com/xiaomeng9597/files/releases/download/files/rtl8367b.tar.gz
-tar -xvf rtl8367b.tar.gz
-
-# openwrt主线rtl8367b驱动资源包，暂时使用这样替换
-# wget https://github.com/xiaomeng9597/files/releases/download/files/rtl8367b-openwrt.tar.gz
-# tar -xvf rtl8367b-openwrt.tar.gz
-
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
