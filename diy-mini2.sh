bd973d488aa40c0a7ac70960cbb7215d565c97b3
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-12-10 07:26:35 +0800

    Update diy-mini2.sh

--- diy-mini2.sh
+++ diy-mini2.sh
@@ -22,6 +22,7 @@ function git_sparse_clone() {
   branch="$1" repourl="$2" && shift 2
   git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
   repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  echo $repodir
   cd $repodir && git sparse-checkout set $@
   mv -f $@ ../package
   cd .. && rm -rf $repodir
