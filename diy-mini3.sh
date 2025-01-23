0cae24ab7e8bdd1ca9d477783624230c59eb715e
519561910@qq.com <519561910@qq.com>
2025-01-23 20:09:11 +0800

    修改 design 为默认主题

--- diy-mini3.sh
+++ diy-mini3.sh
@@ -104,7 +104,7 @@ sed -i 's/invalid users = root/#&/g' feeds/packages/net/samba4/files/smb.conf.te
 
 # 修改 design 为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-sed -i 's/Bootstrap Theme/Design Theme/g' feeds/luci/collections/*/Makefile
+sed -i 's/Bootstrap theme/Design theme/g' feeds/luci/collections/*/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/*/Makefile
 
 # 最大连接数修改为65535
